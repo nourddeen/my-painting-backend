ae19d86549d9dbbbe7ddac9bf0166a5abbee0bb4
Nourrdeen Elyazji <nd.elyazji@gmail.com>
2022-03-29 17:45:49 -0400

    finger crossed

--- models/painting.js
+++ models/painting.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const paintingSchema = new Schema({
     panitingTitle: { type: String, required: true },
     artistName: { type: String, required: true },
-    image: { type: String, required: true},
+    image: { type: String},
     panitingHight :{ type: Number},
     panitingWidth :{ type: Number},
     user: {type: Schema.Types.ObjectId, ref: 'User'},
